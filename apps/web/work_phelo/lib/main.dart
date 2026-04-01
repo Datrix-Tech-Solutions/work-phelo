2581d87e4aa54f6353e1cb58692d1fa86fc21b5c
Henry Agyemang <amglna2020@gnail.com>
2026-04-01 12:04:13 +0000

    fix(web): enable path URL strategy and fix tenantSlug from login response

--- apps/web/work_phelo/lib/main.dart
+++ apps/web/work_phelo/lib/main.dart
@@ -1,9 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'routings/go_routes.dart';
+import 'package:flutter_web_plugins/url_strategy.dart';
 import 'work_phelo_components/theme/app.colors.dart';
 
 void main() {
+  usePathUrlStrategy();
   runApp(ProviderScope(child: const MyApp()));
 }
 
